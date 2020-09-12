3e07a7c69c39a4154ad3072f20e6765dd739b37f
AycanOcal <aycanocal28@gmail.com>
2020-09-12 03:38:44 +0300

    Get movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const API_KEY = "79b40ce";
 
 function MovieDetails({ movieTitle }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${API_KEY}`)
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+      });
+  }, []);
   return (
     <div>
       <h2>Movie Details</h2>
-      <h3>{movieTitle}</h3>
+      {typeof data != "undefined" ? (
+        <div>
+          <dl key={data.imdbID}>
+            <dt>Title:</dt>
+            <dd>{data.Title}</dd>
+            <dt>Year:</dt>
+            <dd>{data.Year}</dd>
+            <dt>Imdb ID:</dt>
+            <dd> {data.imdbID}</dd>
+          </dl>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
